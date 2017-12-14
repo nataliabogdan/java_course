b7c6cf334099aa8c62eece9e30c88e24fa6d6db0
Natalia Bogdan <plopik.tala@gmail.com>
2017-12-14 15:08:33 +0100

    Math examples

--- sandbox/src/main/java/plopik/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/plopik/sandbox/MyFirstProgram.java
@@ -3,6 +3,21 @@ package plopik.sandbox;
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-    }
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
 
+        System.out.println(1.0 / 2);
+        System.out.println("2" + "2"); //concatenation used
+        System.out.println("2" + 2); //concatenation
+        System.out.println(2 + "2");//concatenation
+        System.out.println(2 + 2 * 2);
+        System.out.println("2 + 2 = " + (2 + 2)); //concatenation
+    }
 }
+
+
+
+
+
