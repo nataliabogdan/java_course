c0c2576314f707737f09edff15e800aa11e35c6e
Natalia Bogdan <plopik.tala@gmail.com>
2018-01-13 12:09:34 +0100

    Optomized navigation between pages

--- addressbook-web-tests/src/test/java/plopik/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/plopik/addressbook/appmanager/NavigationHelper.java
@@ -13,15 +13,26 @@ public class NavigationHelper extends BaseHelper {
         //different selectors
         //wd.findElement(By.xpath("//li[contains(@class,'admin')]/a")).click(); //first variant
         //wd.findElement(By.xpath("//ul//a[contains(@href, 'group.php')]")).click(); //second variant
-        click(By.xpath("//a[@href='group.php']"));
+
+
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        } else {
+            click(By.xpath("//a[@href='group.php']"));
+        }
 
         //wd.findElementByXPath("//a[@href='group.php']").click(); //third variant
     }
 
     public void gotoContactPage() {
-        click(By.xpath("//a[contains(text(),'home')]"));
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        } else {
+            click(By.xpath("//a[contains(text(),'home')]"));
+        }
     }
-
 
     public void returnToGroupPage() {
         click(By.linkText("group page"));
@@ -30,4 +41,6 @@ public class NavigationHelper extends BaseHelper {
     public void returnToContactPage() {
         click(By.xpath("//a[contains(text(), 'home')]"));
     }
+
+
 }
