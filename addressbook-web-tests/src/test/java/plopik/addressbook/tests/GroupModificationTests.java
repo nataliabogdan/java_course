73a58794c2fb48439f031c5ce84ce0c628b9c587
Natalia Bogdan <plopik.tala@gmail.com>
2018-02-12 21:06:47 +0100

    Addressbook. Added verification of array

--- addressbook-web-tests/src/test/java/plopik/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/plopik/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import plopik.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -13,15 +14,20 @@ public class GroupModificationTests extends TestBase {
         app.getNavigationHelper().gotoGroupPage();
         if (!app.getGroupHelper().isGroupExist()) {
             app.getGroupHelper().createGroup(new GroupData("group1", null, "edit3"));
-
         }
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("group1", "edit2", "edit3"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"group1", "edit2", "edit3"); //сохраняем старый идентификатор
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);//удаляем из списка изменяемую группу
+        before.add(group);// добавляем в список новую отредактированную группу
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));// преобразуем списки в множества и сравниваем
+
     }
 }
