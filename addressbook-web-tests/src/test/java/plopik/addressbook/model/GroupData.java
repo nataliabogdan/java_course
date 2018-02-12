a1b66569e499be758053637127e0afd0f84198da
Natalia Bogdan <plopik.tala@gmail.com>
2018-02-12 21:23:16 +0100

    Addressbox. Added verification of lists for GroupCreation test

--- addressbook-web-tests/src/test/java/plopik/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/plopik/addressbook/model/GroupData.java
@@ -3,12 +3,12 @@ package plopik.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -16,7 +16,7 @@ public class GroupData {
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -45,7 +45,7 @@ public class GroupData {
         return Objects.hash(id, name);
     }
 
-    public String getId() {
+    public Integer getId() {
 
         return id;
     }
@@ -62,4 +62,7 @@ public class GroupData {
         return footer;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- addressbook-web-tests/src/test/java/plopik/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/plopik/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import plopik.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -13,9 +14,21 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("group1", null, "edit3"));
+        GroupData group = new GroupData("group1", null, "edit3");
+        app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+
+
+        int max = 0;
+        for (GroupData g: after) { // цикл по всем элементам
+            if (g.getId() > max){
+                max = g.getId(); //мак присваеваем новый айди
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));// преобразуем списки в множества и сравниваем
 
     }
 }
